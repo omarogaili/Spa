8d6c69861d9ba55a9b2196222a52d070594d0a57
omar ogaili <ogialiomar@hotmail.com>
2026-04-17 23:35:50 +0200

    addPackage_Should_Return_SuccessMessage is green as ecpected

--- src/main/java/com/Spa/spa/Services/PackageServices.java
+++ src/main/java/com/Spa/spa/Services/PackageServices.java
@@ -1,28 +1,38 @@
 package com.Spa.spa.Services;
 
+import org.springframework.data.mongodb.core.MongoOperations;
+
+import com.Spa.spa.models.Package;
+
 public class PackageServices implements IPackageServices {
-        @Override
-        public String addPackage(com.Spa.spa.models.Package spaPackage) {
-            return null;
-        }
-    
-        @Override
-        public String updatePackage(com.Spa.spa.models.Package spaPackage) {
-            return null;
-        }
-    
-        @Override
-        public String deletePackage(String id) {
-            return null;
-        }
-    
-        @Override
-        public com.Spa.spa.models.Package getPackageById(String id) {
-            return null;
-        }
-    
-        @Override
-        public Iterable<com.Spa.spa.models.Package> getAllPackages() {
-            return null;
-        }
+    private MongoOperations mongoOperations;
+
+    public PackageServices(MongoOperations mongoOperations) {
+        this.mongoOperations = mongoOperations;
+    }
+    @Override
+    public String addPackage(Package spaPackage) {
+        mongoOperations.save(spaPackage);
+        return "Package added successfully: " + spaPackage.getName();
+    }
+
+    @Override
+    public String updatePackage(Package spaPackage) {
+        return null;
+    }
+
+    @Override
+    public String deletePackage(String id) {
+        return null;
+    }
+
+    @Override
+    public Package getPackageById(String id) {
+        return null;
+    }
+
+    @Override
+    public Iterable<Package> getAllPackages() {
+        return null;
+    }
 }
--- src/test/java/com/Spa/spa/PackageServicesTest.java
+++ src/test/java/com/Spa/spa/PackageServicesTest.java
@@ -1,9 +1,13 @@
 package com.Spa.spa;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoOperations;
 
 import com.Spa.spa.Services.PackageServices;
 import com.Spa.spa.models.Package;
@@ -11,12 +15,23 @@ import com.Spa.spa.models.Package;
 @SpringBootTest
 public class PackageServicesTest {
     private PackageServices packageServices;
+    private MongoOperations mongoOperations;
+
+    @BeforeEach
+    public void setUp() {
+        mongoOperations = org.mockito.Mockito.mock(MongoOperations.class);
+        packageServices = new PackageServices(mongoOperations);
+    }
 
     @Test
     public void addPackage_Should_Return_SuccessMessage() {
         Package spaPackage = new Package("Relaxation Package", "feeling Good", 200);
+        when(mongoOperations.save(spaPackage)).thenReturn(spaPackage);
+        when(mongoOperations.findById(spaPackage.getId(), Package.class)).thenReturn(spaPackage);
+        System.out.println(" *********** Package ID: " + spaPackage.getName()+ " ***********");
+
         String result = packageServices.addPackage(spaPackage);
         assertNotNull(result);
-        assert result.equals("Package added successfully");
+        assertEquals("Package added successfully: " + spaPackage.getName(), result);
     }
 }
