4f248030149bfaaab17bab08b279e28ddb9f3670
omar ogaili <ogialiomar@hotmail.com>
2026-04-20 02:06:00 +0200

    feat: I-10 add JWT Service, TokenFilter and securityFilterChain

---
+++ src/main/java/com/Spa/spa/Services/JwtService.java
@@ -0,0 +1,51 @@
+package com.Spa.spa.Services;
+
+import java.security.Key;
+import java.util.Date;
+
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+@Service
+public class JwtService {
+    private String SECRET = "gVPU7e3UZJueR3tnrktjWA==";
+    private final Key key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(SECRET));
+
+    public String generateToken (String userName, String role){
+        return Jwts.builder().setSubject(userName)
+                    .claim("role", role)
+                    .setIssuedAt(new Date())
+                    .setExpiration(new Date(System.currentTimeMillis()+ 1000* 60 * 60)) // 1h
+                    .signWith(key)
+                    .compact();
+    }
+
+    public Claims validateToken(String token){
+        try{
+            return Jwts.parserBuilder()
+                       .setSigningKey(key)
+                       .build()
+                       .parseClaimsJws(token)
+                       .getBody();
+        } catch(Exception e){
+            return null;
+        }
+    }
+
+    public String readRoleClaim(String token){
+        Claims claims = validateToken(token);
+        if (claims == null) {
+            return null;
+        }
+        return claims.get("role", String.class);
+    }
+
+    public String readSubject(String token){
+        Claims claims = validateToken(token);
+        return claims != null ? claims.getSubject(): null;
+    }
+}
--- src/test/java/com/Spa/spa/OrderServicesTest.java
+++ src/test/java/com/Spa/spa/OrderServicesTest.java
@@ -9,6 +9,7 @@ import java.time.LocalDate;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.mongodb.core.MongoOperations;
 
