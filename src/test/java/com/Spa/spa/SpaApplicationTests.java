13f735d763cabe2ed09cefd22f7541324c95940c
omar ogaili <ogialiomar@hotmail.com>
2026-04-23 01:47:06 +0200

    fix: spring boot test dose not work as expected in github actions

--- src/test/java/com/Spa/spa/SpaApplicationTests.java
+++ src/test/java/com/Spa/spa/SpaApplicationTests.java
@@ -1,11 +1,7 @@
 package com.Spa.spa;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
-@ActiveProfiles("test")
 
 class SpaApplicationTests {
 
